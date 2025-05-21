5102d2e7cb92559bf1112e6605a5f620b9c6615b
Federico Andres Jacome Castaneda <fedeandresdeveloper@gmail.com>
2025-05-21 23:49:48 +0200

    Created few migrations, seed some data and basic Villa view and controller

---
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public VillaController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
