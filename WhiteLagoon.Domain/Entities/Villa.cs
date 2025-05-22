39c70a2da047bf4fca39134753d2530a22b7d539
Federico Andres Jacome Castaneda <fedeandresdeveloper@gmail.com>
2025-05-22 23:13:00 +0200

    Created create Villa

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,15 +1,29 @@
-﻿namespace WhiteLagoon.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Domain.Entities
 {
     public class Villa
     {
         public int Id { get; set; }
+
         public required string Name { get; set; }
+
         public string? Description { get; set; }
+
+        [Display(Name = "Price per night")]
+        [Range(10, 100000)]
         public double Price { get; set; }
+
         public int Sqft { get; set; }
+
+        [Range(1,10)]
         public int Occupancy { get; set; }
+
+        [Display(Name="Image url")]
         public string? ImageUrl { get; set; }
+
         public DateTime Created_Date { get; set; }
+
         public DateTime? Updated_Date { get; set;}
     }
 }
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -15,6 +16,24 @@ namespace WhiteLagoon.Web.Controllers
         {
             var villas = _db.Villas.ToList();
             return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(villa);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
